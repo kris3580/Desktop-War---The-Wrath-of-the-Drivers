4bd0259658115a310a82050dfbda0dd09da8f086
kris3580 <145036874+kris3580@users.noreply.github.com>
2024-10-11 14:05:23 +0300

    Basic Enemy Movement

--- Desktop War - The Wrath of the Drivers/Assets/Scripts/Enemy/Enemy.cs
+++ Desktop War - The Wrath of the Drivers/Assets/Scripts/Enemy/Enemy.cs
@@ -1,6 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
 
+    [SerializeField] List<Vector3> targetPositions;
+    [SerializeField] List<bool> hasMovementTargetPositionStarted;
+    [SerializeField] List<bool> hasMovementTargetPositionReached;
+
+    private float timer = 0f;
+    private Vector3 startingPosition;
+    [SerializeField] float speed;
+
+
+    private void Start()
+    {
+        startingPosition = transform.position;
+        StartCoroutine(EnemyMovementSequence());
+    }
+
+    private void Update()
+    {
+        
+
+        if (hasMovementTargetPositionStarted[0] && !hasMovementTargetPositionReached[0])
+            timer += Time.deltaTime * speed;
+        {
+            transform.position = Vector3.Lerp(startingPosition, targetPositions[0], timer);
+
+            if (timer >= 1) hasMovementTargetPositionReached[0] = true;
+        }
+
+    }
+
+    private IEnumerator EnemyMovementSequence()
+    {
+        yield return new WaitForSeconds(9f);
+        hasMovementTargetPositionStarted[0] = true;
+
+    }
 }
---
+++ Desktop War - The Wrath of the Drivers/Assets/Scripts/Enemy/EnemyTools.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class EnemyTools : MonoBehaviour
+{
+    public static void MoveEnemyToPoint(Vector3 targetPosition)
+    {
+        
+    }
+
+}
